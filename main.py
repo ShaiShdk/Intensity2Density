8fd12ecc6255cae7a514543be737df204160a1e6
ShaiShad <47192449+ShaiShdk@users.noreply.github.com>
2022-07-21 21:42:17 -0700

    Delete main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-# Testing
-
-import Intensity2Density as I2D
-import matplotlib.pyplot as plt
-
-path_to_image = 'Intensity_profile.tiff'
-img = plt.imread(path_to_image)
-I2D.Convert_Pattern_to_Points(img, 200 , [10,10])
